a0b79c0d6dfae18f278bc84ee725575321d9e656
鸟人1号 <1944996484@qq.com>
2019-08-23 09:51:47 +0800

    非线性模型，利用神经网络进行预测，画出样例和预测值分布图像

--- 非线性回归.py
+++ 非线性回归.py
@@ -21,7 +21,7 @@ y = tf.placeholder(tf.float32, [None, 1])
 Weights_L1 = tf.Variable(tf.random_normal([1, 10]))
 biases_L1 = tf.Variable(tf.zeros([1, 10]))
 Wx_plus_b_L1 = tf.matmul(x, Weights_L1) + biases_L1
-L1 = tf.nn.tanh(Wx_plus_b_L1)
+L1 = tf.nn.tanh(Wx_plus_b_L1)  # 激活函数
 
 # 定义神经网络输出层
 Weights_L2 = tf.Variable(tf.random_normal([10, 1]))
