65deb8f3518737290f9079b5e7a2ec649700315f
outman123 <1944996484@qq.com>
2019-09-18 14:33:24 +0800

    批处理文件来进行模型训练

---
+++ tfrecord/train.bat
@@ -0,0 +1,9 @@
+python F:/git_code/slim/train_image_classifier.py ^
+--train_dir=F:/git_code/slim/model ^
+--dataset_name=myimages ^
+--dataset_split_name=train ^
+--dataset_dir=F:/git_code/slim/images
+--batch_size=10 ^
+--max_number_of_steps=10000 ^
+--model_name=inception_v3 ^
+pause
