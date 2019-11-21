5de1511e3def8516a2ba335205bc888c7f79492f
outman123 <1944996484@qq.com>
2019-11-21 17:56:15 +0800

    Update freeze.py

--- mnist_quant/freeze.py
+++ mnist_quant/freeze.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, ReLU
 from tensorflow.keras import backend as K 
 from tensorflow.contrib.quantize import experimental_create_eval_graph 
 
-# build the model
+# 构建模型
 input_tensor = Input(shape=(28, 28, 1), name='input_tensor')
 x = Conv2D(32, (3, 3), name='conv1')(input_tensor)
 x = ReLU(name='relu1')(x)
@@ -23,26 +23,26 @@ model = Model(inputs=input_tensor, outputs=output_tensor)
 
 sess = tf.compat.v1.keras.backend.get_session()
 
-# add fake quantize ops to the eval garph
+# 添加伪量化节点到eval garph
 experimental_create_eval_graph(input_graph=sess.graph, 
                                weight_bits=8, 
                                activation_bits=8)
 
-# check if add the fake quantize ops successfully
+
 for node in sess.graph.as_graph_def().node:
     if 'AssignMaxLast' in node.name or 'AssignMinLast' in node.name:
         print('node name: {}'.format(node.name))
 
-# load the quantize-aware trained model weights
+# 加载quantize-aware 训练的模型的权重
 saver = tf.train.Saver()
 saver.restore(sess, './models/quant_aware_trained/model.ckpt')
 
-# freeze the graph
+# 固化图
 const_graph = tf.graph_util.convert_variables_to_constants(
     sess=sess,
     input_graph_def=sess.graph.as_graph_def(),
     output_node_names=['output_tensor/BiasAdd']) 
 
-# serialize and dump the freezed graph to a pb file
+# 将固化图序列化保存成pb文件
 with tf.gfile.GFile('./models/frozen1.pb', "wb") as f:
     f.write(const_graph.SerializeToString())
