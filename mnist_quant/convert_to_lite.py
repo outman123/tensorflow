ffe5b9c8b1c24660d6883efba1766eaf8949179d
outman123 <1944996484@qq.com>
2019-11-21 17:57:29 +0800

    Update convert_to_lite.py

--- mnist_quant/convert_to_lite.py
+++ mnist_quant/convert_to_lite.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 from tensorflow.lite.python import lite 
 FLAGS = tf.app.flags.FLAGS
 
-# build a converter
+# 构建一个转换器
 converter = lite.TFLiteConverter.from_frozen_graph(
     graph_def_file='./models/frozen.pb', 
     input_arrays=['input_tensor'], 
     output_arrays=['output_tensor/BiasAdd'], 
     input_shapes={'input_tensor': [1, 28, 28, 1]})
 
-# set some attributes
+# 设置属性
 converter.inference_type = tf.uint8 
 converter.inference_input_type = tf.uint8
 converter.post_training_quantize = False
@@ -17,7 +17,7 @@ input_arrays = converter.get_input_arrays()
 converter.quantized_input_stats = {input_arrays[0]: (0., 1.)}
 converter.default_ranges_stats = (0, 6)
 
-# convert the pd model and write to tflite file
+#将pb文件转化成tflite文件
 tflite_quantized_model = converter.convert()
 with open('models/model1.tflite', 'wb') as f:
     f.write(tflite_quantized_model)
